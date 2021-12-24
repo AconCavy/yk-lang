d319eb14c1cb2e2dbd44d0640e826ac54d09cb27
AconCavy <aconcavy@gmail.com>
2021-12-25 03:50:18 +0900

    Add TokenType.cs

---
+++ src/YKLang/TokenType.cs
@@ -0,0 +1,50 @@
+﻿namespace YKLang;
+
+public enum TokenType
+{
+    // Single characters
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Semicolon,
+
+    // Single or Double characters
+    Bang,
+    BangEqual,
+    Assign,
+    Equal,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literal
+    Identifier,
+    String,
+    Number,
+
+    // Keywords
+    And,
+    Or,
+    Class,
+    If,
+    Else,
+    While,
+    For,
+    Nil,
+    Return,
+    This,
+    Base,
+    Var,
+    Function,
+
+    // Special
+    Eof
+}
