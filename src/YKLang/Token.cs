fcd61b12fb3343950ba4d94e9e50e9b07012f217
AconCavy <aconcavy@gmail.com>
2021-12-25 03:50:29 +0900

    Add Token.cs

---
+++ src/YKLang/Token.cs
@@ -0,0 +1,8 @@
+﻿namespace YKLang;
+
+public class Token
+{
+    public TokenType Type { get; }
+    public string Value { get; }
+    public Token(TokenType type, string value) => (Type, Value) = (type, value);
+}
