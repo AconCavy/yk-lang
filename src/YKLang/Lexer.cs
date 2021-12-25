43e6c567b891fc416db237fad7a1581316709e75
AconCavy <aconcavy@gmail.com>
2021-12-25 10:09:57 +0900

    Add Lexer and tests

---
+++ src/YKLang/Lexer.cs
@@ -0,0 +1,155 @@
+﻿namespace YKLang;
+
+public static class Lexer
+{
+    public static IEnumerable<Token> GenerateTokens(string source)
+    {
+        var current = 0;
+        var tokens = new List<Token>();
+        ReadOnlySpan<char> ignoreCharacters = stackalloc char[] { ' ', '\t', '\r', '\n' };
+        var keywords = new Dictionary<string, TokenType>
+        {
+            { "class", TokenType.Class },
+            { "if", TokenType.If },
+            { "else", TokenType.Else },
+            { "while", TokenType.While },
+            { "for", TokenType.For },
+            { "nil", TokenType.Nil },
+            { "return", TokenType.Return },
+            { "this", TokenType.This },
+            { "base", TokenType.Base },
+            { "var", TokenType.Var },
+            { "function", TokenType.Function }
+        };
+
+        while (current < source.Length)
+        {
+            if (ignoreCharacters.Contains(source[current]))
+            {
+                current++;
+                continue;
+            }
+
+            var (type, length) = GetTypeAndLength(source.AsSpan(current));
+            if (length < 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var start = current;
+            current += length;
+
+            if (type is TokenType.String)
+            {
+                start++;
+                length -= 2;
+            }
+
+            var literal = source.AsSpan(start, length).ToString();
+            if (type is TokenType.Other)
+            {
+                type = keywords.ContainsKey(literal) ? keywords[literal] : TokenType.Identifier;
+            }
+
+            tokens.Add(new Token(type, literal));
+        }
+
+        return tokens;
+    }
+
+    private static (TokenType TokenType, int Length) GetTypeAndLength(ReadOnlySpan<char> source)
+    {
+        return source[0] switch
+        {
+            '(' => (TokenType.LeftParen, 1),
+            ')' => (TokenType.RightParen, 1),
+            '{' => (TokenType.LeftBrace, 1),
+            '}' => (TokenType.RightBrace, 1),
+            ',' => (TokenType.Comma, 1),
+            '.' => (TokenType.Dot, 1),
+            '+' => (TokenType.Plus, 1),
+            '-' => (TokenType.Minus, 1),
+            '*' => (TokenType.Star, 1),
+            '/' => (TokenType.Slash, 1),
+            ';' => (TokenType.Semicolon, 1),
+            '#' => (TokenType.Hash, CommentLength(source)),
+            '!' => IsMatch(source, "!=") ? (TokenType.BangEqual, 2) : (TokenType.Bang, 1),
+            '=' => IsMatch(source, "==") ? (TokenType.Equal, 2) : (TokenType.Assign, 1),
+            '<' => IsMatch(source, "<=") ? (TokenType.LessEqual, 2) : (TokenType.Less, 1),
+            '>' => IsMatch(source, ">=") ? (TokenType.GreaterEqual, 2) : (TokenType.Greater, 1),
+            '"' => (TokenType.String, StringLength(source)),
+            >= '0' and <= '9' => (TokenType.Number, NumberLength(source)),
+            '&' => IsMatch(source, "&&") ? (TokenType.And, 2) : (TokenType.BitwiseAnd, 1),
+            '|' => IsMatch(source, "||") ? (TokenType.Or, 2) : (TokenType.BitwiseOr, 1),
+            _ => (TokenType.Other, OtherLength(source))
+        };
+    }
+
+    public static bool IsMatch(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
+    {
+        return target.Length <= source.Length && source[..target.Length].SequenceEqual(target);
+    }
+
+    public static int CommentLength(ReadOnlySpan<char> source)
+    {
+        for (var i = 0; i < source.Length; i++)
+        {
+            if (source[i] == '\n')
+            {
+                return i + 1;
+            }
+        }
+
+        return source.Length;
+    }
+
+    public static int StringLength(ReadOnlySpan<char> source)
+    {
+        for (var i = 1; i < source.Length; i++)
+        {
+            if (source[i] == '"')
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int NumberLength(ReadOnlySpan<char> source)
+    {
+        var dot = false;
+        for (var i = 0; i < source.Length; i++)
+        {
+            if (char.IsDigit(source[i]))
+                continue;
+            if (source[i] == '.')
+            {
+                if (!dot)
+                    dot = true;
+                else if (i + 1 < source.Length && char.IsDigit(source[i + 1]))
+                    return -1;
+                else
+                    return i;
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+        return source.Length;
+    }
+
+    public static int OtherLength(ReadOnlySpan<char> source)
+    {
+        ReadOnlySpan<char> end = "(){},.+-*/!=<>&| \t\r\n";
+        for (var i = 0; i < source.Length; i++)
+        {
+            if (end.Contains(source[i]))
+                return i;
+        }
+
+        return source.Length;
+    }
+}
---
+++ test/YKLang.Tests/LexerTests.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace YKLang.Tests;
+
+public class LexerTests
+{
+    [Fact]
+    public void SingleCharacterTokensTest()
+    {
+        const string Source = "(){},.+-*/;";
+        var expected = new List<Token>
+        {
+            new(TokenType.LeftParen, "("),
+            new(TokenType.RightParen, ")"),
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.RightBrace, "}"),
+            new(TokenType.Comma, ","),
+            new(TokenType.Dot, "."),
+            new(TokenType.Plus, "+"),
+            new(TokenType.Minus, "-"),
+            new(TokenType.Star, "*"),
+            new(TokenType.Slash, "/"),
+            new(TokenType.Semicolon, ";"),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void HashTokenTest()
+    {
+        const string Source = "# Comment 1\n{}# Comment 2\n";
+        var expected = new List<Token>
+        {
+            new(TokenType.Hash, "# Comment 1\n"),
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.RightBrace, "}"),
+            new(TokenType.Hash, "# Comment 2\n"),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void DoubleCharactersTokensTest()
+    {
+        const string Source = "! != = == < <= > >=";
+        var expected = new List<Token>
+        {
+            new(TokenType.Bang, "!"),
+            new(TokenType.BangEqual, "!="),
+            new(TokenType.Assign, "="),
+            new(TokenType.Equal, "=="),
+            new(TokenType.Less, "<"),
+            new(TokenType.LessEqual, "<="),
+            new(TokenType.Greater, ">"),
+            new(TokenType.GreaterEqual, ">="),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void StringTokenTest()
+    {
+        const string Source = "{\"This is String\"}";
+        var expected = new List<Token>
+        {
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.String, "This is String"),
+            new(TokenType.RightBrace, "}"),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void NumberTokenTest()
+    {
+        const string Source = "{0.5.}";
+        var expected = new List<Token>
+        {
+            new(TokenType.LeftBrace, "{"),
+            new(TokenType.Number, "0.5"),
+            new(TokenType.Dot, "."),
+            new(TokenType.RightBrace, "}"),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void KeywordTokenTest()
+    {
+        const string Source = "class foo";
+        var expected = new List<Token>
+        {
+            new(TokenType.Class, "class"),
+            new(TokenType.Identifier, "foo"),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void DefineVariableTest()
+    {
+        const string Source = "var x = 1.0";
+        var expected = new List<Token>
+        {
+            new(TokenType.Var, "var"),
+            new(TokenType.Identifier, "x"),
+            new(TokenType.Assign, "="),
+            new(TokenType.Number, "1.0"),
+        };
+
+        var actual = Lexer.GenerateTokens(Source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("HelloWorld", "Hello", true)]
+    [InlineData("HelloWorld", "World", false)]
+    [InlineData("Hell", "Hello", false)]
+    public void IsMatchTest(string source, string target, bool expected)
+    {
+        var actual = Lexer.IsMatch(source, target);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("# Comment", 9)]
+    [InlineData("# Comment\n", 10)]
+    [InlineData("# Comment\n{}", 10)]
+    public void CommentLengthTest(string source, int expected)
+    {
+        var actual = Lexer.CommentLength(source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("\"\"", 2)]
+    [InlineData("\"String\"", 8)]
+    [InlineData("\"A\nB\"", 5)]
+    public void StringLengthTest(string source, int expected)
+    {
+        var actual = Lexer.StringLength(source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("0", 1)]
+    [InlineData("1000", 4)]
+    [InlineData("0.5", 3)]
+    [InlineData("5.5.5", -1)]
+    public void NumberLengthTest(string source, int expected)
+    {
+        var actual = Lexer.NumberLength(source);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("class Foo", 5)]
+    [InlineData("if ()", 2)]
+    [InlineData("foo", 3)]
+    public void OtherLengthTest(string source, int expected)
+    {
+        var actual = Lexer.OtherLength(source);
+        Assert.Equal(expected, actual);
+    }
+}
