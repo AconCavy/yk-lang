7ad3fb5b7f466ffbb98c0512c5a3d2d5954ca39b
AconCavy <aconcavy@gmail.com>
2022-01-03 00:23:36 +0900

    Modify ParseException.cs

--- src/YKLang/ParseException.cs
+++ src/YKLang/ParseException.cs
@@ -1,3 +1,14 @@
 ﻿namespace YKLang;
 
-public class ParseException : Exception { }
+[Serializable]
+public class ParseException : Exception
+{
+    public ParseException() : base() { }
+    public ParseException(string message) : base(message) { }
+    public ParseException(string message, Exception inner) : base(message, inner) { }
+
+    protected ParseException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context)
+    {
+    }
+}
