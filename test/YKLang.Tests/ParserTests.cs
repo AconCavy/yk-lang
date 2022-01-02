d408d70e8b2d50af27e1696599bbc7057e3745b1
AconCavy <aconcavy@gmail.com>
2022-01-03 03:09:25 +0900

    Add ParserTests.cs

---
+++ test/YKLang.Tests/ParserTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace YKLang.Tests;
+
+public class ParserTests
+{
+    [Theory]
+    [InlineData("1 + 2", "(+ 1 2)")]
+    [InlineData("-123 * (45.67)", "(* (- 123) (group 45.67))")]
+    [InlineData("1 + 2 * 3 - 4", "(- (+ 1 (* 2 3)) 4)")]
+    [InlineData("1 * 3 - 4 / -2", "(- (* 1 3) (/ 4 (- 2)))")]
+    public void CalculationAstTest(string source, string expected)
+    {
+        var tokens = Lexer.Analyze(source);
+        var expression = Parser.Parse(source, tokens);
+
+        var astBuilder = new AstStringBuilder(source);
+        var actual = astBuilder.ToString(expression);
+        Assert.Equal(expected, actual);
+    }
+}
