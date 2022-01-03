0ed46479cf0123d1e937bbd656f1ecc92c62640f
AconCavy <aconcavy@gmail.com>
2022-01-04 06:40:18 +0900

    Modify Function.cs

--- src/YKLang/Statements/Function.cs
+++ src/YKLang/Statements/Function.cs
@@ -3,8 +3,15 @@
 public class Function : Statement
 {
     public Token Name { get; }
-    public Token[]? Params { get; }
-    public Statement[]? Body { get; }
+    public Token[] Params { get; }
+    public Statement[] Body { get; }
+
+    public Function(Token name, IEnumerable<Token> @params, IEnumerable<Statement> body)
+    {
+        Name = name;
+        Params = @params.ToArray();
+        Body = body.ToArray();
+    }
 
     public override T Accept<T>(IVisitor<T> visitor)
     {
