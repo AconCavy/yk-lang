31f18bbdbb455782e413df1bec2a317de4cd7b57
AconCavy <aconcavy@gmail.com>
2022-01-08 08:12:48 +0900

    Add InterpretableObject.cs

---
+++ src/YKLang/InterpretableObject.cs
@@ -0,0 +1,15 @@
+﻿using YKLang.Statements;
+
+namespace YKLang;
+
+public class InterpretableObject
+{
+    public string Source { get; }
+    public Statement[] Statements { get; }
+
+    public InterpretableObject(string source, IEnumerable<Statement> statements)
+    {
+        Source = source;
+        Statements = statements as Statement[] ?? statements.ToArray();
+    }
+}
