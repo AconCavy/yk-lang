958a6127ae9f8bba92dddfcab6a8def1af7f23c2
AconCavy <aconcavy@gmail.com>
2022-01-08 06:53:08 +0900

    Add ParsableObject.cs

---
+++ src/YKLang/ParsableObject.cs
@@ -0,0 +1,13 @@
+﻿namespace YKLang;
+
+public class ParsableObject
+{
+    public string Source { get; }
+    public IReadOnlyList<Token> Tokens { get; }
+
+    public ParsableObject(string source)
+    {
+        Source = source;
+        Tokens = Lexer.Analyze(Source);
+    }
+}
