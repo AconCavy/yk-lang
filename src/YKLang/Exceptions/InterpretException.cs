6776f7ba5f4f086d0cfa7441c40f0bc484002472
AconCavy <aconcavy@gmail.com>
2022-01-08 05:53:59 +0900

    Add InterpretException.cs

---
+++ src/YKLang/Exceptions/InterpretException.cs
@@ -0,0 +1,14 @@
+﻿namespace YKLang.Exceptions;
+
+[Serializable]
+public class InterpretException : Exception
+{
+    public InterpretException() { }
+    public InterpretException(string message) : base(message) { }
+    public InterpretException(string message, Exception inner) : base(message, inner) { }
+
+    protected InterpretException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context)
+    {
+    }
+}
