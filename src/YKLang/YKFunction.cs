6b99603120a43526ad6af5cb48ad6dfcb18d2e34
AconCavy <aconcavy@gmail.com>
2022-01-11 22:31:32 +0900

    Add YKFunction.cs

---
+++ src/YKLang/YKFunction.cs
@@ -0,0 +1,47 @@
+﻿using YKLang.Statements;
+
+namespace YKLang;
+
+public class YKFunction : IYKCallable
+{
+    public string Name { get; }
+    private readonly Function _declaration;
+    private readonly Environment _environment;
+    private readonly bool _isInitializer;
+
+    public YKFunction(string name, Function declaration, Environment environment, bool isInitializer)
+    {
+        Name = name;
+        _declaration = declaration;
+        _environment = environment;
+        _isInitializer = isInitializer;
+    }
+
+    public YKFunction Bind(YKInstance instance)
+    {
+        var env = new Environment(_environment);
+        env.Define("this", instance);
+        return new YKFunction(Name, _declaration, env, _isInitializer);
+    }
+
+    public int Arity()
+    {
+        return _declaration.Parameters.Length;
+    }
+
+    public dynamic? Call(Interpreter interpreter, IEnumerable<dynamic> arguments)
+    {
+        var env = new Environment(_environment);
+        foreach (var (parameter, argument) in _declaration.Parameters.Zip(arguments))
+        {
+            env.Define(interpreter.Source[parameter.Range], argument);
+        }
+
+        return null;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
